c8c13a130a7c0efdac45de9be463ce612ac2eb69
Josh <zylo.codes@gmail.com>
2023-03-09 18:54:42 -0600

    save

--- hello_world.zig
+++ hello_world.zig
@@ -4,9 +4,19 @@ const Vec2 = struct{
   x: f64 = 0.0,
   y: f64,
   z: f64,
+
+  fn print(self: Vec2) void {
+    std.debug.print("self\n{}\n", .{self});
+    std.debug.print("x: {}\ny: {}\nz: {}\n", .{self.x, self.y, self.z});
+  }
+
+  fn incrementX(self: Vec2) void {
+    self.x = self.x + 1;
+  }
 };
 
 pub fn main() void {
   const v = Vec2{.y = 1.0, .z = 2.0};
-  std.debug.print("v: {}", .{v});
+  v.incrementX();
+  v.print();
 }
