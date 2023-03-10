974013f5058814a93fb0e3b50d5136bd44198698
Josh <zylo.codes@gmail.com>
2023-03-10 17:14:08 -0600

    LikeAnObject

--- hello_world.zig
+++ hello_world.zig
@@ -1,22 +1,14 @@
 const std = @import("std");
 
-const Vec2 = struct{
-  x: f64 = 0.0,
-  y: f64,
-  z: f64,
+const LikeAnObject = struct{
+  value: i32,
 
-  fn print(self: Vec2) void {
-    std.debug.print("self\n{}\n", .{self});
-    std.debug.print("x: {}\ny: {}\nz: {}\n", .{self.x, self.y, self.z});
-  }
-
-  fn incrementX(self: Vec2) void {
-    self.x = self.x + 1;
+  fn print(self: *LikeAnObject) void {
+    std.debug.print("value: {}\n", .{self.value});
   }
 };
 
 pub fn main() void {
-  const v = Vec2{.y = 1.0, .z = 2.0};
-  v.incrementX();
-  v.print();
+  var obj = LikeAnObject{.value = 47};
+  obj.print();
 }
