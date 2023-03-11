6e714ad29353c77af9c968a2ad676f096af67224
Josh <zylo.codes@gmail.com>
2023-03-10 18:12:26 -0600

    array

--- hello_world.zig
+++ hello_world.zig
@@ -1,14 +1,14 @@
-// https://gist.github.com/ityonemo/769532c2017ed9143f3571e5ac104e50#enums
+// https://gist.github.com/ityonemo/769532c2017ed9143f3571e5ac104e50#arrays-and-slices
+
 const std = @import("std");
 
-const EnumType = enum(i32){
-  EnumOne,
-  EnumTwo,
-  EnumThree = 3
-};
-
 pub fn main() void {
-  std.debug.print("One: {}\n", .{EnumType.EnumOne});
-  std.debug.print("Two: {}\n", .{EnumType.EnumTwo});
-  std.debug.print("Three: {}\n", .{@enumToInt(EnumType.EnumThree) == 3});
+  var array: [3]u32 = [3]u32{47,47, 47};
+
+  // also valid:
+  // var array = [_]u32{47, 47, 47};
+
+  // var invalid = array[4]; // error: index 4 outside array of size 3
+  std.debug.print("array[0]: {}\n", .{array[0]});
+  std.debug.print("length: {}\n", .{array.len});
 }
